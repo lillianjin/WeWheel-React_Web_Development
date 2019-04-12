6711a5d30702f56bb8dcd4952227001676e3beb2
Lu Jin <lillianjin67@gmail.com>
2019-04-12 15:45:00 -0500

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import './Home.css';
-import NavBar from '../NavBar/NavBar.jsx'
+import NavBar from '../NavBar/NavBar.jsx';
+import MyCarousel from './MyCarousel.jsx';
 
 
 class Home extends Component {
 
   render() {
       return(
-        <NavBar/>
+        <div>
+          <NavBar/>
+          <MyCarousel/>
+          <div className="home-bg-filter"></div>
+          <h1 className="home-slogan">
+          SHARE A CAR TODAY!
+          </h1>
+        </div>
     )
   }
-
 }
 
 
-export default Home
+export default Home;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,46 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from './node_modules/react-router-dom'
 import './NavBar.css';
 import logo from '../assets/logo.gif'
 import { faSignInAlt, faHome, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Navbar,  Nav } from 'react-bootstrap';
 
 class NavBar extends Component {
 
   render() {
       return(
-        <div className='NavBar'>
-          <div className='header'>
-          <Link to = '/'>
-            <img className="logo" src={logo} alt="logo"/>
-          </Link>
-          <h1 id='wewheel'>WeWheel</h1>
-          <ul id="navbar">
-            <li className="active">
-              <FontAwesomeIcon className="icon" icon={faHome} size="sm" />
-              <a href="#section1" id="nav1" className="nav">HOME</a>
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faSignInAlt} size="sm" />
-              <a href="#section2" id="nav2" className="nav">LOGIN</a>
-            </li>
-            <li>
-              <FontAwesomeIcon className="icon" icon={faUserPlus} size="sm" />
-              <a href="#section3" id="nav3" className="nav">SIGNUP</a>
-            </li>
-          </ul>
-          </div>
+        <div>
+          <Navbar bg="dark" variant="dark" className="justify-content-between">
+            <Navbar.Brand href="/">
+              <img alt="logo" src={logo} width="30" height="30"
+                className="logo"/>
+              <p className="wewheel"> WeWheel</p>
+            </Navbar.Brand>
+            <Nav activeKey="/">
+              <Nav.Item>
+                <Nav.Link href="/">
+                  <FontAwesomeIcon className="icon" icon={faHome} size="sm" />
+                  {" HOME"}
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/signup">
+                  <FontAwesomeIcon className="icon" icon={faSignInAlt} size="sm" />
+                  {" LOGIN"}
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/login">
+                  <FontAwesomeIcon className="icon" icon={faUserPlus} size="sm" />
+                  {" SIGNUP"}
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar>
         </div>
-    )
+      );  
   }
-
 }
 
-
-export default NavBar
+export default NavBar;
