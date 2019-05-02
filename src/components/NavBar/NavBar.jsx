47c1c8d8e3303a9449cfd777c2f1c050ee6b30f9
Ao <aotymatty96@outlook.com>
2019-05-02 14:59:36 -0500

    Update NavBar with Authentication

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 // import { Link } from './node_modules/react-router-dom'
 import './NavBar.css';
 import logo from '../assets/logo.gif'
-import { faSignInAlt, faHome, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faSignInAlt, faHome, faUserPlus, faUserCircle, faSignOutAlt} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navbar,  Nav } from 'react-bootstrap';
-
+import  Authentication  from '../Authentication/Authentication.js'
 class NavBar extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.onLogOut = this.onLogOut.bind(this);
+
+  }
+
+  onLogOut(){
+    Authentication.logout();
+    this.forceUpdate()
+
+    // this.props.history.push( '/',null);
+  }
+
   render() {
+      // console.log(Authentication.isLoggedIn())
       return(
         <div>
           <Navbar bg="dark" variant="dark" className="justify-content-between">
@@ -24,18 +39,42 @@ class NavBar extends Component {
                   {" HOME"}
                 </Nav.Link>
               </Nav.Item>
+
+              { Authentication.isLoggedIn() &&
+              <Nav.Item>
+                <Nav.Link href="#/profile">
+                  <FontAwesomeIcon className="icon" icon={faUserCircle } size="sm" />
+                  {" PROFILE"}
+                </Nav.Link>
+              </Nav.Item>
+              }
+
+              { Authentication.isLoggedIn() &&
+             <Nav.Item>
+               <Nav.Link onClick = {this.onLogOut}>
+                 <FontAwesomeIcon className="icon" icon={faSignOutAlt} size="sm" />
+                 {" LOGOUT"}
+               </Nav.Link>
+             </Nav.Item>
+             }
+
+              { !Authentication.isLoggedIn() &&
               <Nav.Item>
                 <Nav.Link href="#/login">
                   <FontAwesomeIcon className="icon" icon={faSignInAlt} size="sm" />
                   {" LOGIN"}
                 </Nav.Link>
               </Nav.Item>
+              }
+
+              { !Authentication.isLoggedIn() &&
               <Nav.Item>
                 <Nav.Link href="#/register">
                   <FontAwesomeIcon className="icon" icon={faUserPlus} size="sm" />
                   {" SIGNUP"}
                 </Nav.Link>
               </Nav.Item>
+              }
             </Nav>
           </Navbar>
         </div>
