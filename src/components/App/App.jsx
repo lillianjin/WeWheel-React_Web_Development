9dec8842a2b3c111b880c47f3b7f523d1060fe07
Xiaolu85 <xlqizju@gmail.com>
2019-05-02 15:03:43 -0500

    usefile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {HashRouter as Router, Route, Switch} from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 // Include your new Components here
@@ -8,6 +8,7 @@ import Register from '../Register/Register.jsx';
 import Login from '../Login/Login.jsx';
 import RentCar from '../RentCar/RentCar.jsx';
 import Posts from '../Posts/Posts.jsx';
+import Userfile from '../Userfile/Userfile.jsx';
 
 class App extends Component {
   render() {
@@ -15,10 +16,11 @@ class App extends Component {
       <div id="root">
         <Router>
           <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/register" component={Register}/>
-            <Route path="/login" component={Login}/>
-            <Route exact path="/rent" component={RentCar}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route exact path="/rent" component={RentCar} />
+            <Route path="/user/:userId" component={Userfile} />
           </Switch>
         </Router>
       </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Button, Input,Card, Form, Grid, Header, Image, Message, Segment, Divider, Icon, Checkbox} from 'semantic-ui-react'
-import { Link,Redirect } from 'react-router-dom'
+import { Button, Input, Card, Form, Grid, Header, Image, Message, Segment, Divider, Icon, Checkbox } from 'semantic-ui-react'
+import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
 import logo from '../assets/logo.gif'
 import background from '../assets/backgroundimg.jpg'
@@ -8,107 +8,113 @@ import NavBar from '../NavBar/NavBar.jsx'
 import Home from '../Home/Home.jsx'
 import Choices from '../Home/Choices.jsx'
 import login from './login.scss'
+import Authentication from '../Authentication/Authentication.js'
 
 
 
 
 class Login extends Component {
-      constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state ={
-            user:{
-                username:'',
-                password:'',
-                email:''
-            },
-            isLoggedin:false,
-            message:'',
-            redirect:false,
-        }
+    this.state = {
+      user: {
+        username: '',
+        password: '',
+        email: ''
+      },
+      isLoggedin: false,
+      message: '',
+      redirect: false,
+    }
 
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onChangePassword = this.onChangePassword.bind(this);
-        this.onChangeUserName = this.onChangeUserName.bind(this)
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeUserName = this.onChangeUserName.bind(this)
 
-      }
+  }
 
-      onSubmit(event){
-        event.preventDefault();
-        // create HTTP body message
-        axios.post('http://localhost:4000/api/users/login', {
-          password: this.state.user.password,
-          UserName:this.state.user.username
-        })
-        .then((response) => {
-          console.log("Login successfully")
-          console.log(response);
-          this.setState({isLoggedin:true});
-          this.props.history.push( '/',null)
-          console.log(this.state)
-        })
-        .catch((error) => {
-          console.log("Unable to login")
-          console.log(error);
-        });
+  onSubmit(event) {
+    event.preventDefault();
+    // create HTTP body message
+    axios.post('http://localhost:4000/api/users/login', {
+      password: this.state.user.password,
+      UserName: this.state.user.username
+    })
+      .then((response) => {
+        console.log("Login successfully")
+        console.log(response);
+        this.setState({ isLoggedin: true });
+        var t1 = Authentication.isLoggedIn();
+        console.log(t1);
+        Authentication.login(this.state.user.username);
+        var t2 = Authentication.isLoggedIn();
+        console.log(t2);
+        this.props.history.push('/', null)
+        console.log(this.state)
+      })
+      .catch((error) => {
+        console.log("Unable to login")
+        console.log(error);
+      });
 
-      }
+  }
 
-      onChangePassword(event){
-        const user = this.state.user;
-        user.password = event.target.value;
-        this.setState({
-          user
-        })
-      }
+  onChangePassword(event) {
+    const user = this.state.user;
+    user.password = event.target.value;
+    this.setState({
+      user
+    })
+  }
 
-      onChangeUserName(event){
-        const user = this.state.user;
-        user.username = event.target.value;
-        this.setState({
-          user
-        })
-      }
+  onChangeUserName(event) {
+    const user = this.state.user;
+    user.username = event.target.value;
+    this.setState({
+      user
+    })
+  }
 
-      render() {
-        console.log("render")
-        if(this.state.redirect){
-            return(
-              <Redirect to = {
-                {
-                  pathname:'/',
-                  state:{isLoggedIn:false}
-                }
-              }/>
-            )
-        } else{
-            return (
-              <div>
-                    <NavBar isLoggedIn = {false}/>
-                    <img className = "loginBackgroundimg" src = {background}/>
-                    <div className = "logInBox">
-                            <Header as='h2' textAlign='center' color = 'black'>
-                            <Icon name = 'sign in'/>Login Your Account!
+  render() {
+    console.log("render")
+    if (this.state.redirect) {
+      return (
+        <Redirect to={
+          {
+            pathname: '/',
+            state: { isLoggedIn: false }
+          }
+        } />
+      )
+    } else {
+      return (
+        <div>
+          <NavBar isLoggedIn={false} />
+          <img className="loginBackgroundimg" src={background} />
+          <div className="logInBox">
+            <Header as='h2' textAlign='center' color='black'>
+              <Icon name='sign in' />Login Your Account!
                             </Header>
-                            <Form size='large' onSubmit={this.onSubmit}>
-                                <Form.Field>
-                                  <label>Username</label>
-                                  <input placeholder='Your Username' onChange={this.onChangeUserName} required />
-                                </Form.Field>
-                                <Form.Field>
-                                  <label>Password</label>
-                                  <input placeholder='Password' type = "password" onChange={this.onChangePassword} required/>
-                                </Form.Field>
-                                <Button color = 'black' fluid size='large' type='submit'>Log in</Button>
-                            </Form>
-                            <Message> Don't have an account? <Link to="/register">Sign up now!</Link> </Message>
-                            <Message> Forget your password? <Link to="/register">Reset here!</Link> </Message>
-                    </div>
-              </div>
-            )
-        }
+            <Form size='large' onSubmit={this.onSubmit}>
+              <Form.Field>
+                <label>Username</label>
+                <input placeholder='Your Username' onChange={this.onChangeUserName} required />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input placeholder='Password' type="password" onChange={this.onChangePassword} required />
+              </Form.Field>
+              <Button color='black' fluid size='large' type='submit'>Log in</Button>
+            </Form>
+            <Message> Don't have an account? <Link to="/register">Sign up now!</Link> </Message>
+            <Message> Forget your password? <Link to="/register">Reset here!</Link> </Message>
+          </div>
+        </div>
+      )
+    }
 
-      }
+  }
 
 }
 
