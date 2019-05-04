c7a119c4c8db98cbe0259d16e3dce7470262e590
Xiaolu85 <xlqizju@gmail.com>
2019-05-04 00:38:04 -0500

    Updates

---
+++ precache-manifest.8695e0b045978711710446819c0f4831.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8c4761b08d2c484bce6",
+    "url": "/Xiaolu85/WeWheel/static/css/main.61b3d708.chunk.css"
+  },
+  {
+    "revision": "f8c4761b08d2c484bce6",
+    "url": "/Xiaolu85/WeWheel/static/js/main.fcbc01c7.chunk.js"
+  },
+  {
+    "revision": "e5d9a93a704305f01bd8",
+    "url": "/Xiaolu85/WeWheel/static/js/runtime~main.6d9f722c.js"
+  },
+  {
+    "revision": "622918209ae4d2350b24",
+    "url": "/Xiaolu85/WeWheel/static/css/2.ab156208.chunk.css"
+  },
+  {
+    "revision": "622918209ae4d2350b24",
+    "url": "/Xiaolu85/WeWheel/static/js/2.b76747f5.chunk.js"
+  },
+  {
+    "revision": "d0bc1989f1432b969a6602ce2edf3926",
+    "url": "/Xiaolu85/WeWheel/static/media/backgroundimg.d0bc1989.jpg"
+  },
+  {
+    "revision": "e36f20584bd403515897a0365ec29a7f",
+    "url": "/Xiaolu85/WeWheel/static/media/logo.e36f2058.gif"
+  },
+  {
+    "revision": "8f70a8ab78b46a5e6bbe2a237885e91a",
+    "url": "/Xiaolu85/WeWheel/static/media/pic1.8f70a8ab.jpg"
+  },
+  {
+    "revision": "888a0a150f1daf4d041f0ae352804fae",
+    "url": "/Xiaolu85/WeWheel/static/media/pic2.888a0a15.jpg"
+  },
+  {
+    "revision": "96347ede6fe3aa39a72947a6f28c59b1",
+    "url": "/Xiaolu85/WeWheel/static/media/pic3.96347ede.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Xiaolu85/WeWheel/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Xiaolu85/WeWheel/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Xiaolu85/WeWheel/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Xiaolu85/WeWheel/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Xiaolu85/WeWheel/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Xiaolu85/WeWheel/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Xiaolu85/WeWheel/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Xiaolu85/WeWheel/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Xiaolu85/WeWheel/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Xiaolu85/WeWheel/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Xiaolu85/WeWheel/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Xiaolu85/WeWheel/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Xiaolu85/WeWheel/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Xiaolu85/WeWheel/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Xiaolu85/WeWheel/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Xiaolu85/WeWheel/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "f23a0692f17bd5212f9a6393f9ebc850",
+    "url": "/Xiaolu85/WeWheel/index.html"
+  }
+];
